b00a5f9ec46fad873ae92b7825d23141e6d82ab8
Seth Maisel <seth.maisel@gmail.com>
2019-10-01 02:34:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -16,7 +16,7 @@ def id_generator
 end
 
 def my_hash_creator(key, value)
-  my_hash_creator_hash_creator = {:[] => ()}
+  my_hash_creator_hash_creator = {:() => ()}
 end
 
 def read_from_hash(hash, key)
